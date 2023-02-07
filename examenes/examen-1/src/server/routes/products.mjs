fd844af66d33fc2a4e1d43a0d91acfb099610759
RED-G4898 <red_g4898@proton.me>
2023-02-07 10:48:46 -0600

    Enhancement: Add axios node package
    Added Axios to project in order to handle http requests from the client
    side.
    
    References: DAPP2-11

--- examenes/examen-1/src/server/routes/products.mjs
+++ examenes/examen-1/src/server/routes/products.mjs
@@ -83,4 +83,27 @@ router.patch('/products/update/:barcode', async (req, res) => {
     }
 });
 
+router.delete('/products/delete/:barcode', async (req, res) => {
+    try {
+        const product = await Product.deleteOne({barcode : req.params.barcode});
+        res.status(200).json({
+            status : '200 Ok! ✓',
+            data : {
+                product
+            }
+        });
+        console.log('Product added to database!');
+    } catch (error) {
+        res.status(400).json({
+            status : 'Request failed! ✕',
+            message : error
+        });
+        res.status(500).json({
+            status : 'Server error! ✕',
+            message : error
+        });
+        console.log("There's an error with the request");
+    }
+})
+
 export default router;
