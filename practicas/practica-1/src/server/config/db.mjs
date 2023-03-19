adea634b79b266fec2d65a0bdaebe641204d41a4
RED-G4898 <red_g4898@proton.me>
2023-03-18 18:04:09 -0600

    feature: Implement config from past project (examen-1)
    Copied config files from previous project (examen-1) in order to simplify
    server initialization and db connection.
    
    references: DAPP2-10 - DAPP2-17

--- practicas/practica-1/src/server/config/db.mjs
+++ practicas/practica-1/src/server/config/db.mjs
@@ -1,27 +1,28 @@
-// npm modules
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// DB connection data
 const USER = process.env.DB_USER;
-const PASSWD = process.env.DB_PASSWORD;
+const PASSWORD = process.env.DB_PASSWORD;
 const CLUSTER = process.env.DB_CLUSTER;
-const URI = `mongodb+srv://${USER}:${PASSWD}@${CLUSTER}/?retryWrites=true&w=majority`;
-const OPT = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: 'fas'
+
+const uri = `mongodb+srv://${USER}:${PASSWORD}@${CLUSTER}/?retryWrites=true&w=majority`;
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'fas'
 }
 
-// DB connection
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(URI, OPT);
-    console.info('DB Connected! ✓');
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri, options);
+        console.log("Database connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 }
+
+
+export default connectDB;
--- practicas/practica-1/src/server/config/server.mjs
+++ practicas/practica-1/src/server/config/server.mjs
@@ -1,24 +1,22 @@
-// npm modules
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-
-// project modules
-import { connectDB } from './db.mjs';
-import router from './../routes/index.mjs';
+import connectDB from './db.mjs';
+import router from '../routes/index.mjs';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.SV_PORT;
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
+const PORT = process.env.SERVER_PORT;
+
 connectDB();
 
 app.listen(PORT, () => {
-  console.info(`Server running on port: ${PORT}!`);
+    console.log(`Server is running on port ${PORT}`);
 });
